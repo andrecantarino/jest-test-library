958263c2443e5eadffdce7393dd4263ab753915b
ancantarino <cantarino.andre@gmail.com>
2021-08-21 12:59:24 -0300

    when disabled checkbox, button turns to gray

--- src/App.test.js
+++ src/App.test.js
@@ -36,3 +36,36 @@ test('checkbox disabled button on first click and enables on second click', () =
   fireEvent.click(checkbox);
   expect(button).toBeEnabled();
 });
+
+test('Disabled button has gray background and reverts to red', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ backgroundColor: 'gray' });
+
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('Clicked disabled button has gray background and reverts to blue', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+
+  // change button to blue
+  fireEvent.click(button);
+
+  // disabled button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ backgroundColor: 'gray' });
+
+  // re-enabled button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+
